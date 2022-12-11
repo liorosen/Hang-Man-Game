560de5af52fde095fdd2406a963bc097f3903d23
lioro <l1i9o9r6@gmail.com>
2022-12-12 00:20:24 +0200

    edit database path

--- TopicChosing.java
+++ TopicChosing.java
@@ -3,22 +3,22 @@ import java.util.Scanner;
 
 public class TopicChosing {
     public static File ChoseTopic(int chosenNumber) {
-        File file = new File("/../DataBases/welcome.txt/");
+        File file = new File("/C:/Users/lioro/IdeaProjects/HangMan/src/DataBases/welcome.txt/");
         switch (chosenNumber) {
             case 1:
-                file = new File("/../DataBases/Celebrities.txt/");
+                file = new File("/C:/Users/lioro/IdeaProjects/HangMan/src/DataBases/Celebrities.txt/");
                 break;
             case 2:
-                file = new File("/../DataBases/Countries.txt/");
+                file = new File("/C:/Users/lioro/IdeaProjects/HangMan/src/DataBases/Countries.txt/");
                 break;
             case 3:
-                file = new File("/../DataBases/movies.txt/");
+                file = new File("/C:/Users/lioro/IdeaProjects/HangMan/src/DataBases/movies.txt/");
                 break;
             case 4:
-                file = new File("/../DataBases/Musician and Artists.txt/");
+                file = new File("/C:/Users/lioro/IdeaProjects/HangMan/src/DataBases/Musician and Artists.txt/");
                 break;
             case 5:
-                file = new File("/../DataBases/TV Series.txt/");
+                file = new File("/C:/Users/lioro/IdeaProjects/HangMan/src/DataBases/TV Series.txt/");
                 break;
         }
         return file;
